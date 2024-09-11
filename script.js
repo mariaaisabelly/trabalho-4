ed29e321c73c02031f2172d0eeaf2ed890373f4a
mariaaisabelly <0000109086839xsp@al.educacao.sp.gov.br>
2024-09-11 11:45:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('botao').onclick = function() {
+    document.getElementById('mensagem').innerHTML = 'Você clicou no botão!';
+};
+direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerText
+    canvas.style.filter = "blur(2px)"
+}
+
+const gameLoop = () => {
+    clearInterval(loopId)
+
+    ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
+    moveSnake()
+    drawSnake()
+    chackEat()
+    checkCollision()
+
+    loopId = setTimeout(() => {
+        gameLoop()
+    }, 300)
+}
+
+gameLoop()
+
+document.addEventListener("keydown", ({ key }) => {
+    if (key == "ArrowRight" && direction != "left") {
+        direction = "right"
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = "left"
+    }
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = "down"
+    }
+
+    if (key == "ArrowUp" && direction != "down") {
+        direction = "up"
+    }
+})
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00"
+    menu.style.display = "none"
+    canvas.style.filter = "none"
+
+    snake = [initialPosition]
+})
